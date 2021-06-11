e153afb2279e717016e60aca502f740a83c638eb
SN-Vidya <snvidya1998@gmail.com>
2021-06-11 13:21:28 +0530

    Payment gateway

--- payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
@@ -1,11 +1,12 @@
 package com.shoppingcartsystem.paymentservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
