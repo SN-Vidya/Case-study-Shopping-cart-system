26af67e94dd2afc32e7d9867c872be4f02e9144e
SN-Vidya <snvidya1998@gmail.com>
2021-06-16 13:29:21 +0530

    Cart-Service

--- cart-service/src/main/java/com/shoppingcartsystem/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/shoppingcartsystem/cartservice/CartServiceApplication.java
@@ -1,14 +1,15 @@
 package com.shoppingcartsystem.cartservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
