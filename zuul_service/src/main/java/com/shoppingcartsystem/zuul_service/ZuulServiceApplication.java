426734148feccf53cd328d043e0988b1ad77b1fc
SN-Vidya <snvidya1998@gmail.com>
2021-06-16 13:25:10 +0530

    api gateway

--- zuul_service/src/main/java/com/shoppingcartsystem/zuul_service/ZuulServiceApplication.java
+++ zuul_service/src/main/java/com/shoppingcartsystem/zuul_service/ZuulServiceApplication.java
@@ -1,17 +1,24 @@
 package com.shoppingcartsystem.zuul_service;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
-
+	
+	
 }
+
