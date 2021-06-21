f7e2425d71c28bd1dfa0b8a25f128720da0ce154
SN-Vidya <snvidya1998@gmail.com>
2021-06-21 10:32:31 +0530

    Order-service

--- order-service/src/main/java/com/shoppingcartsystem/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/shoppingcartsystem/orderservice/OrderServiceApplication.java
@@ -1,11 +1,15 @@
 package com.shoppingcartsystem.orderservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,12 +24,23 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 	
-	@Bean 
-	
-	public Docket swaggerConfiguratin() { return new
-			  Docket(DocumentationType.SWAGGER_2) .select()
-			  		.apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.orderservice"))
-			  		.build(); }
-
+	@Bean
+	public  Docket swaggerConfiguration()  {
+            return new Docket(DocumentationType.SWAGGER_2)  
+                .select() 
+                .apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.orderservice.controller"))
+                .paths(PathSelectors.any())                          
+                .build()
+                .apiInfo(apiDetails());
+	}
+    private ApiInfo apiDetails() {
+	        return  new ApiInfo("OrderController Api",
+			"Api Collections",
+			null,
+			null,
+			new springfox.documentation.service.Contact(null, null, null),
+			null,null,Collections.emptyList());
+    }
+			
 
 }
