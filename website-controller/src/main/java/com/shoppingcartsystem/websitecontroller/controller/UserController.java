7941c754833e3c87827c605fb8c6883cf98802a4
SN-Vidya <snvidya1998@gmail.com>
2021-06-21 10:30:10 +0530

    website controller

--- website-controller/src/main/java/com/shoppingcartsystem/websitecontroller/controller/UserController.java
+++ website-controller/src/main/java/com/shoppingcartsystem/websitecontroller/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.shoppingcartsystem.websitecontroller.controller;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+//import java.util.List;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -21,39 +21,27 @@ import com.shoppingcartsystem.websitecontroller.model.Cart;
 import com.shoppingcartsystem.websitecontroller.model.Order;
 import com.shoppingcartsystem.websitecontroller.model.Product;
 import com.shoppingcartsystem.websitecontroller.model.User;
-import com.shoppingcartsystem.websitecontroller.repository.CartRepository;
-import com.shoppingcartsystem.websitecontroller.repository.OrderRepository;
-import com.shoppingcartsystem.websitecontroller.repository.ProductRepository;
-import com.shoppingcartsystem.websitecontroller.repository.UserRepository;
-//import com.shoppingcartsystem.websitecontroller.repository.UserRepository;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/web")
 public class UserController {
 	
 	@Autowired
 	 private RestTemplate restTemplate;
 	
-	@Autowired
-	private ProductRepository productRepository ;
-	
-	
-	@Autowired
-	private UserRepository userRepository ;
-	
-	@Autowired
-	private CartRepository cartRepository;
-	
-	@Autowired 
-	private OrderRepository orderRepository;
 	
 	//gives particular product
-	 @RequestMapping(value="/findAllProducts/{productName}",method = RequestMethod.GET)
-	 public List<Product> getAllProducts(@PathVariable("productName") String productName) {
+	
+	 @RequestMapping(value = "/findAllProducts/{productName}",method = RequestMethod.GET)
+	  public String getAllProducts(@PathVariable("productName") String productName)
+	     {
+	         HttpHeaders headers = new HttpHeaders();
+	         headers.setContentType(MediaType.APPLICATION_JSON);
+	         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	         HttpEntity<Product> entity = new  HttpEntity<Product>(headers);
+	         return restTemplate.exchange("http://localhost:8082/product/findAllProducts/"+ productName, HttpMethod.GET, entity, String.class).getBody();
 
-	 restTemplate.getForObject("http://localhost:8082/product/findAllProducts/" + productName ,Product[].class);			
-			return productRepository.findByProductName(productName);
-		}
+	     }
 	 
 	 //gives all products
 	 @RequestMapping(value="/allproducts", method = RequestMethod.GET)
@@ -64,107 +52,124 @@ public class UserController {
 	 return response.getBody();
 	        }
 	 
-	 @RequestMapping(value="/Category/{category}",method = RequestMethod.GET)
-	 public List<Product> getProductsByCategory(@PathVariable("category") String category) {
+	 //gives products of particular category
+	 @RequestMapping(value = "/Category/{category}",method = RequestMethod.GET)
+	  public String getProductsbyCategory(@PathVariable("category") String category)
+	     {
+	         HttpHeaders headers = new HttpHeaders();
+	         headers.setContentType(MediaType.APPLICATION_JSON);
+	         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	         HttpEntity<Product> entity = new  HttpEntity<Product>(headers);
+	         return restTemplate.exchange("http://localhost:8082/product/Category/"+ category, HttpMethod.GET, entity, String.class).getBody();
 
-	 restTemplate.getForObject("http://localhost:8082/product/Category/" + category ,Product[].class);			
-			return productRepository.findByCategory(category);
-		}
+	     }
 	 
-	 @RequestMapping(value="/users/{_id}",method = RequestMethod.GET)
-	 public Optional<User> getById(@PathVariable("_id") String _id) {
-
-	 restTemplate.getForObject("http://localhost:8081/profile/users/" + _id ,User.class);			
-			return userRepository.findById(_id);
-		}
+//	 @RequestMapping(value="/Category/{category}",method = RequestMethod.GET)
+//	 public List<Product> getProductsByCategory(@PathVariable("category") String category) {
+//
+//	 restTemplate.getForObject("http://localhost:8082/product/Category/" + category ,Product[].class);			
+//			return productRepository.findByCategory(category);
+//		}
 	 
 	 
-	 @RequestMapping(value="/userName/{fullName}", method = RequestMethod.GET)
-	 public User getByFullNmae(@PathVariable("fullName") String fullName) {
+	 
+//	 @RequestMapping(value="/users/{_id}",method = RequestMethod.GET)
+//	 public Optional<User> getById(@PathVariable("_id") String _id) {
+//
+//	 restTemplate.getForObject("http://localhost:8081/user/users/" + _id ,User.class);			
+//			return userRepository.findById(_id);
+//		}
+	 
+	 
+	 @RequestMapping(value = "/userName/{fullName}",method = RequestMethod.GET)
+	  public String getByFullName(@PathVariable("fullName") String fullName)
+	     {
+	         HttpHeaders headers = new HttpHeaders();
+	         headers.setContentType(MediaType.APPLICATION_JSON);
+	         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	         HttpEntity<User> entity = new  HttpEntity<User>(headers);
+	         return restTemplate.exchange("http://localhost:8081/user/userName/"+ fullName, HttpMethod.GET, entity, String.class).getBody();
 
-	 restTemplate.getForObject("http://localhost:8081/profile/userName/" + fullName ,User.class);			
-			return userRepository.findByFullName(fullName);
-		}
+	     }
+	 
+	 
+//	 @RequestMapping(value="/userName/{fullName}", method = RequestMethod.GET)
+//	 public User getByFullNmae(@PathVariable("fullName") String fullName) {
+//
+//	 restTemplate.getForObject("http://localhost:8081/user/userName/" + fullName ,User.class);			
+//			return userRepository.findByFullName(fullName);
+//		}
 
-     @RequestMapping(value = "/customerRegistration",method = RequestMethod.POST)
+     @RequestMapping(value = "/Register",method = RequestMethod.POST)
      public String addNewProfile(@RequestBody  User user)
      {
          HttpHeaders headers = new HttpHeaders();
          headers.setContentType(MediaType.APPLICATION_JSON);
          headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
          HttpEntity<User> entity = new  HttpEntity<User>(user,headers);
-         return restTemplate.exchange("http://localhost:8081/profile/customerRegistration", HttpMethod.POST, entity, String.class).getBody();
+         return restTemplate.exchange("http://localhost:8081/user/Register", HttpMethod.POST, entity, String.class).getBody();
 
      }
      
-     @RequestMapping(value = "/delete/{_id}",method = RequestMethod.DELETE)
-     public String deleteById(@PathVariable("_id") String _id)
-     {
-         HttpHeaders headers = new HttpHeaders();
-         headers.setContentType(MediaType.APPLICATION_JSON);
-         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-         HttpEntity<User> entity = new  HttpEntity<User>(headers);
-         return restTemplate.exchange("http://localhost:8081/profile/delete/"+ _id, HttpMethod.DELETE, entity, String.class).getBody();
-
-     }
+    
+   
+//     @RequestMapping(value="/allcart/{cartId}",method = RequestMethod.GET)
+//	 public Cart getByCartId(@PathVariable("cartId") int cartId) {
+//
+//	 restTemplate.getForObject("http://localhost:8083/CartService/allcart/" + cartId ,Cart.class);			
+//			return cartRepository.findByCartId(cartId);
+//		}
      
-     @RequestMapping(value = "/update/{email_id}",method = RequestMethod.PUT)
-     public String updateProfile(@PathVariable("email_id") String email_id, @RequestBody User user)
-     {
-         HttpHeaders headers = new HttpHeaders();
-         headers.setContentType(MediaType.APPLICATION_JSON);
-         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-         HttpEntity<User> entity = new  HttpEntity<User>(user,headers);
-         return restTemplate.exchange("http://localhost:8081/profile/update/"+ email_id, HttpMethod.PUT, entity, String.class).getBody();
+     @RequestMapping(value = "/getcart/{cartId}",method = RequestMethod.GET)
+	  public String getByCartId(@PathVariable("cartId") int cartId)
+	     {
+	         HttpHeaders headers = new HttpHeaders();
+	         headers.setContentType(MediaType.APPLICATION_JSON);
+	         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	         HttpEntity<Cart> entity = new  HttpEntity<Cart>(headers);
+	         return restTemplate.exchange("http://localhost:8083/cart/getcart/"+ cartId, HttpMethod.GET, entity, String.class).getBody();
 
-     }
+	     }
      
-     @RequestMapping(value="/allcart/{cartId}",method = RequestMethod.GET)
-	 public Cart getByCartId(@PathVariable("cartId") int cartId) {
-
-	 restTemplate.getForObject("http://localhost:8083/CartService/allcart/" + cartId ,Cart.class);			
-			return cartRepository.findByCartId(cartId);
-		}
-     
-     @RequestMapping(value = "/addCart",method = RequestMethod.POST)
+     @RequestMapping(value = "/additem",method = RequestMethod.POST)
      public String addCart(@RequestBody Cart cart)
      {
          HttpHeaders headers = new HttpHeaders();
          headers.setContentType(MediaType.APPLICATION_JSON);
          headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
          HttpEntity<Cart> entity = new  HttpEntity<Cart>(cart,headers);
-         return restTemplate.exchange("http://localhost:8083/CartService/addCart", HttpMethod.POST, entity, String.class).getBody();
+         return restTemplate.exchange("http://localhost:8083/cart/additem", HttpMethod.POST, entity, String.class).getBody();
 
      }
      
-     @RequestMapping(value = "/deletecart/{_id}",method = RequestMethod.DELETE)
-     public String deleteCartById(@PathVariable("_id") String _id)
+     @RequestMapping(value = "/deletecart/{cartId}",method = RequestMethod.DELETE)
+     public String deleteCartById(@PathVariable("cartId") String cartId)
      {
          HttpHeaders headers = new HttpHeaders();
          headers.setContentType(MediaType.APPLICATION_JSON);
          headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
          HttpEntity<Cart> entity = new  HttpEntity<Cart>(headers);
-         return restTemplate.exchange("http://localhost:8083/CartService/deletecart/"+ _id, HttpMethod.DELETE, entity, String.class).getBody();
+         return restTemplate.exchange("http://localhost:8083/CartService/deletecart/"+ cartId, HttpMethod.DELETE, entity, String.class).getBody();
 
      }
      
-     @RequestMapping(value = "/updatecart/{cartId}",method = RequestMethod.PUT)
-     public String updateCart(@PathVariable("cartId") int cartId, @RequestBody Cart cart)
-     {
-         HttpHeaders headers = new HttpHeaders();
-         headers.setContentType(MediaType.APPLICATION_JSON);
-         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-         HttpEntity<Cart> entity = new  HttpEntity<Cart>(cart,headers);
-         return restTemplate.exchange("http://localhost:8083/CartService/updatecart/"+ cartId, HttpMethod.PUT, entity, String.class).getBody();
-
-     }
+//     @RequestMapping(value = "/updateitem/{cartId}",method = RequestMethod.PUT)
+//     public String updateCart(@PathVariable("cartId") int cartId, @RequestBody Cart cart)
+//     {
+//         HttpHeaders headers = new HttpHeaders();
+//         headers.setContentType(MediaType.APPLICATION_JSON);
+//         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//         HttpEntity<Cart> entity = new  HttpEntity<Cart>(cart,headers);
+//         return restTemplate.exchange("http://localhost:8083/CartService/updatecart/"+ cartId, HttpMethod.PUT, entity, String.class).getBody();
+//
+//     }
      
-     @RequestMapping(value="/customerorder/{customerId}",method = RequestMethod.GET)
-	 public List<Order> getOrderByCustomerId(@PathVariable("customerId") Integer customerId) {
-
-	 restTemplate.getForObject("http://localhost:8084/order-service/customerorder/" + customerId ,Order[].class);			
-			return orderRepository.findOrderByCustomerId(customerId);
-		}
+//     @RequestMapping(value="/customerorder/{customerId}",method = RequestMethod.GET)
+//	 public List<Order> getOrderByCustomerId(@PathVariable("customerId") Integer customerId) {
+//
+//	 restTemplate.getForObject("http://localhost:8084/order-service/customerorder/" + customerId ,Order[].class);			
+//			return orderRepository.findOrderByCustomerId(customerId);
+//		}
      
      @RequestMapping(value = "/addOrder",method = RequestMethod.POST)
      public String addOrder(@RequestBody Order order)
