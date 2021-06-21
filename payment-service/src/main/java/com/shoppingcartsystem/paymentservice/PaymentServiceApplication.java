5515f174b25e4df6d38e036f80cf4ed21b2477aa
SN-Vidya <snvidya1998@gmail.com>
2021-06-21 10:32:00 +0530

    Payment-service

--- payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +17,13 @@ public class PaymentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
+@Bean 
+	
+	public Docket swaggerConfiguratin() { return new
+			  Docket(DocumentationType.SWAGGER_2) .select()
+			  		.apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.orderservice"))
+			  		.build(); }
+
+
 
 }
