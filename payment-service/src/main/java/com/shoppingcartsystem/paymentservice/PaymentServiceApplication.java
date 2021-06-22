2bc36cb404c885595d5d1e921fd871715bb31376
SN-Vidya <snvidya1998@gmail.com>
2021-06-22 21:26:56 +0530

    Payment-gateway

--- payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/shoppingcartsystem/paymentservice/PaymentServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,11 +18,12 @@ public class PaymentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
+	
 @Bean 
 	
 	public Docket swaggerConfiguratin() { return new
 			  Docket(DocumentationType.SWAGGER_2) .select()
-			  		.apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.orderservice"))
+			  		.apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.productservice"))
 			  		.build(); }
 
 
